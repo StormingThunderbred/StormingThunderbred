2acee38c113bc81d75f178e62773dc9d56d6d649
StormingThunderbred <86921622+StormingThunderbred@users.noreply.github.com>
2021-07-11 16:35:13 +0100

    Add files via upload

---
+++ number.py
@@ -0,0 +1,4 @@
+def your_number(number):
+#defines your number
+    print('Your number is', number)
+    
---
+++ salutations(1).py
@@ -0,0 +1,7 @@
+def salutations(name):
+    # prints a nice message to the user
+    #     print("Good morning",name,". How was your day?")
+    print("Good morning {0}!! How was your day?".format(name))
+# prints a nice message to the user
+#  print("Good morning,name,". How was your day?")
+    print("Good morning {0}!! How was your day?".format(name))
