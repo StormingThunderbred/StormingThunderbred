f96c0afc144978922c46f8cd506efdb80daf87f1
StormingThunderbred <86921622+StormingThunderbred@users.noreply.github.com>
2021-07-18 17:48:21 +0100

    Create CODING EXERCISE I.py

---
+++ CODING EXERCISE I.py
@@ -0,0 +1,5 @@
+#convert kg to pounds
+#The conversion rate of kg to pounds is 1kg = 2.205lb
+print("The conversion rate of kg to pounds is 1kg = 2.205lb")
+#multiply by three using this conversion rate
+print(5*2.205)
