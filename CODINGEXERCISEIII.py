d0a370b51ad48b6bec3f5bdcb0f34c68dd685b41
StormingThunderbred <86921622+StormingThunderbred@users.noreply.github.com>
2021-07-25 07:30:47 +0100

    Add files via upload

---
+++ CODINGEXERCISEIII.py
@@ -0,0 +1,61 @@
+def Quiz_Game (name, start):
+    
+    '''Author: Asher
+    Name: Pub Quiz:
+    Version: 1.0
+    Copyright: 2021, Asher Kahtan. All rights reserved.
+    The rules of this game are simple:
+    Type your answer manually into the program.
+    The program will do its best to return your answers, but understand that it is not a guarantee.
+    You may enter one answer and only one.
+    If you get 20 points or more, you win. If you score 0, you lose.'''
+    
+#program must command: ""Please enter your name here: "
+    
+    print("Please enter your name here: ")
+
+#setting condition for starting the game
+    if start == play:
+        copyright = "© Asher, 2021 {0}. All rights reserved.",
+        game_name = "Pub Quiz",
+        message = """\nWelcome to the world of [game_name], [Name of player]!!""",
+        
+#define the questions and their answers
+       
+questions = [
+            "Q1. In the manga series Sailor Moon, what is the name of Usagi's pet cat?"
+            "Q2. How many copies has One Piece sold?"
+            "Q3. Who wrote the German novel Nutcracker & Mouse-King?"
+            ];
+
+choices = [
+          "\nA. Luna \nB. Apollo \nC. Artemis",
+          "\nA 490 million \nB. 720 million \nC. 380 million",
+          "\nA. ETA Hoffman \nB. Goethe \nC. Cornelia Funke",
+          ]; 
+          
+#define response if player chooses right or wrong answer
+if choices == A:
+    print("Congrats, [name of player]. You got 10 points!!")
+else:
+    print("Aww, what a shame. That wasn’t the correct answer. You got 0 points")
+
+#program must summarise points
+
+if total == (numb1 or numb2):
+    print("Bravo, bravo [Name of player]! You’ve won the round!");
+
+else:
+    print("Oh dear. So close and yet so far. Good luck next time [Name of player]!")
+
+#program must display text regardless of outcome
+    
+    print("Would you like to play again?".format(a/b)),
+    A = yes;
+    B = no;
+    
+if answer == B:
+    
+    print("You are the Wiggerzink. Goodbye.");
+    end_game
+    
