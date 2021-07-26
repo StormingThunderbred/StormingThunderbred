5ca723a235e7449605877d8b7ad4a52efe047e30
StormingThunderbred <86921622+StormingThunderbred@users.noreply.github.com>
2021-07-26 20:57:35 +0100

    Add files via upload

---
+++ second_code.py
@@ -0,0 +1,4 @@
+def Cal_force(mass, g):
+    #knowing the mass and gravity you calculate the weight:
+    phy_force = mass * gravity;
+    print("%.2f kg of mass gives a weight of %.2f N." %(mass,gravity)) 
